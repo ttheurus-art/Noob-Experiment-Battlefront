b3e26b56091e11b856d7d0c1520109d2cbbc8e72
Lil BrickBattler <ttheurus@gmail.com>
2026-09-09 18:26:00 +0700

    Enhance mid.lua with warning and Discord features
    Added warning comment and Discord integration to the script.

--- mid.lua
+++ mid.lua
@@ -1,9 +1,12 @@
-local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
    Name = "Red Vs Blue Plane Wars🔥",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "The Red Vs Blue Plane Wars Script!",
+   LoadingTitle = "The Red Vs Blie Plane Wars Script",
    LoadingSubtitle = "By: Theurus_The_Creator",
    Theme = "Ocean", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
@@ -15,7 +18,24 @@ local Window = Rayfield:CreateWindow({
       FolderName = nil, -- Create a custom folder for your hub/game
       FileName = "RedVsBluePlaneWars"
    },
-      
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "https://discord.gg/85cMYuy4J", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = false, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Key System",
+      Subtitle = "To Verify your not a bot, Please type: Key1109888",
+      Note = "Key is required to continue!", -- Use this to tell the user how to get a key
+      FileName = "Keyforent1r11", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = false, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Key1109888"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
+})
 
 local p = Instance.new("Part")
 p.Size = Vector3.new(15, 3, 15)
